a581be9915c0fe8a1a8b450c17833deee0ea3a70
Kurt Wagner <kurt.wagner@prowla.com.au>
2014-03-19 13:28:31 +1100

    Implemented timeAgoShort routine

--- NSDate+TimeAgo.m
+++ NSDate+TimeAgo.m
@@ -11,48 +11,73 @@
 NSLocalizedStringFromTableInBundle(key, @"NSDateTimeAgo", [NSBundle bundleWithPath:[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"NSDateTimeAgo.bundle"]], nil)
 #endif
 
-// shows 1 or two letter abbreviation for units.
-// does not include 'ago' text ... just {value}{unit-abbreviation}
-// does not include interim summary options such as 'Just now'
-- (NSString *)timeAgoSimple
+- (NSString *)timeAgoShort
 {
     NSDate *now = [NSDate date];
     double deltaSeconds = fabs([self timeIntervalSinceDate:now]);
     double deltaMinutes = deltaSeconds / 60.0f;
     
-    int value;
+    int minutes;
     
-    if(deltaSeconds < 60)
+    if(deltaSeconds < 5)
+    {
+        return NSDateTimeAgoLocalizedStrings(@"Now");
+    }
+    else if(deltaSeconds < 60)
     {
         return [self stringFromFormat:@"%%d%@s" withValue:deltaSeconds];
+    }
+    else if(deltaSeconds < 120)
+    {
+        return NSDateTimeAgoLocalizedStrings(@"1m");
     }
     else if (deltaMinutes < 60)
     {
         return [self stringFromFormat:@"%%d%@m" withValue:deltaMinutes];
     }
+    else if (deltaMinutes < 120)
+    {
+        return NSDateTimeAgoLocalizedStrings(@"1h");
+    }
     else if (deltaMinutes < (24 * 60))
     {
-        value = (int)floor(deltaMinutes/60);
-        return [self stringFromFormat:@"%%d%@h" withValue:value];
+        minutes = (int)floor(deltaMinutes/60);
+        return [self stringFromFormat:@"%%d%@h" withValue:minutes];
+    }
+    else if (deltaMinutes < (24 * 60 * 2))
+    {
+        return NSDateTimeAgoLocalizedStrings(@"1d");
     }
     else if (deltaMinutes < (24 * 60 * 7))
     {
-        value = (int)floor(deltaMinutes/(60 * 24));
-        return [self stringFromFormat:@"%%d%@d" withValue:value];
+        minutes = (int)floor(deltaMinutes/(60 * 24));
+        return [self stringFromFormat:@"%%d%@d" withValue:minutes];
+    }
+    else if (deltaMinutes < (24 * 60 * 14))
+    {
+        return NSDateTimeAgoLocalizedStrings(@"1w");
     }
     else if (deltaMinutes < (24 * 60 * 31))
     {
-        value = (int)floor(deltaMinutes/(60 * 24 * 7));
-        return [self stringFromFormat:@"%%d%@w" withValue:value];
+        minutes = (int)floor(deltaMinutes/(60 * 24 * 7));
+        return [self stringFromFormat:@"%%d%@w" withValue:minutes];
+    }
+    else if (deltaMinutes < (24 * 60 * 61))
+    {
+        return NSDateTimeAgoLocalizedStrings(@"1mth");
     }
     else if (deltaMinutes < (24 * 60 * 365.25))
     {
-        value = (int)floor(deltaMinutes/(60 * 24 * 30));
-        return [self stringFromFormat:@"%%d%@mo" withValue:value];
+        minutes = (int)floor(deltaMinutes/(60 * 24 * 30));
+        return [self stringFromFormat:@"%%d%@mths" withValue:minutes];
+    }
+    else if (deltaMinutes < (24 * 60 * 731))
+    {
+        return NSDateTimeAgoLocalizedStrings(@"1yr");
     }
     
-    value = (int)floor(deltaMinutes/(60 * 24 * 365));
-    return [self stringFromFormat:@"%%d%@yr" withValue:value];
+    minutes = (int)floor(deltaMinutes/(60 * 24 * 365));
+    return [self stringFromFormat:@"%%d%@yrs" withValue:minutes];
 }
 
 - (NSString *)timeAgo
@@ -324,14 +349,6 @@ NSLocalizedStringFromTableInBundle(key, @"NSDateTimeAgo", [NSBundle bundleWithPa
                                           timeStyle:tFormatter];
 }
 
-- (NSString *) timeAgoWithLimit:(NSTimeInterval)limit dateFormatter:(NSDateFormatter *)formatter
-{
-    if (fabs([self timeIntervalSinceDate:[NSDate date]]) <= limit)
-        return [self timeAgo];
-
-    return [formatter stringFromDate:self];
-}
-
 // Helper functions
 
 #pragma clang diagnostic push
@@ -357,12 +374,14 @@ NSLocalizedStringFromTableInBundle(key, @"NSDateTimeAgo", [NSBundle bundleWithPa
     
     // Russian (ru)
     if([localeCode isEqual:@"ru"]) {
-        int XY = (int)floor(value) % 100;
+        NSString *valueStr = [NSString stringWithFormat:@"%.f", value];
+        int l = valueStr.length;
+        int XY = [[valueStr substringWithRange:NSMakeRange(l - 2, l)] intValue];
         int Y = (int)floor(value) % 10;
         
-        if(Y == 0 || Y > 4 || (XY > 10 && XY < 15)) return @"";
-        if(Y > 1 && Y < 5 && (XY < 10 || XY > 20))  return @"_";
-        if(Y == 1 && XY != 11)                      return @"__";
+        if(Y == 0 || Y > 4 || XY == 11) return @"";
+        if(Y != 1 && Y < 5)             return @"_";
+        if(Y == 1)                      return @"__";
     }
     
     // Add more languages here, which are have specific translation rules...
